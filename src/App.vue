8edb151afacbcbec146ecda61095175e3f78120b
Serena <serenafgreen@gmail.com>
2024-03-14 00:03:32 -0700

    adds signup component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
     <img alt="Cookable logo" src="https://cookable-api.s3.us-west-1.amazonaws.com/Cookable.svg" style="width: 200px;">
+    <SignupForm/>
     <LoginForm/>
   </div>
 </template>
 
 <script>
 import LoginForm from './components/LoginForm.vue'
-
+import SignupForm from './components/SignupForm.vue'
 
 export default {
   name: 'App',
   components: {
     LoginForm,
+    SignupForm,
   }
 }
 </script>
